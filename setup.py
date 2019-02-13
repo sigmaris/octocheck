e88e2e8e035115f678628ccc0ac33bcdd1fb1971
Hugh Cole-Baker <sigmaris@gmail.com>
2019-02-13 22:13:52 +0000

    Simplify package structure

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ setup(
     author='Hugh Cole-Baker',
     author_email='sigmaris@gmail.com',
     url="https://github.com/sigmaris/octocheck",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    py_modules=['octocheck'],
     install_requires=[
         'github3.py ~= 1.3.0',
     ],
-    entry_points='''
-        [console_scripts]
-        octocheck=octocheck:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'octocheck = octocheck:cli',
+        ]
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
